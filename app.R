c6992c9b14229c13a4d9bc2b9283323dd34af8fd
MONZIOLS <martin.monziols@sante.gouv.fr>
2017-10-25 14:24:14 +0200

    doc + petites fonctionnalités

--- app.R
+++ app.R
@@ -4,8 +4,18 @@ library(shiny)
 
 # L'ordre des éléments dans fluidPage() est le même que l'ordre d'affichage 
 
-ui <- fluidPage("Bonjour à tous!",# On ajoute du texte 
-                
+ui <- fluidPage(tags$h1( "Bonjour à tous!"),# On ajoute du texte 
+                HTML(
+                  '<div class="container-fluid">
+                  <h1>My Shiny App</h1>
+                  <p style="font-family:Impact">
+                  See other apps in the
+                  <a href="http://www.rstudio.com/
+                  products/shiny/shiny-usershowcase/">
+                  Shiny Showcase</a>
+                  </p>
+                  </div>'
+                ),
                 checkboxGroupInput(inputId = "num2",
                                    label =  c("Choix"),
                                    choices = c("choix 1", "choix 2"), 
@@ -13,7 +23,7 @@ ui <- fluidPage("Bonjour à tous!",# On ajoute du texte
                 
                 sliderInput(inputId = "num",
                             label = "Choose a number",
-                            value = 25, min = 1, max = 100),  # ajoute un curseur i.e. un "sliderInput"
+                            value = 25, min = 1, max = 50),  # ajoute un curseur i.e. un "sliderInput"
                
 
                 
@@ -29,6 +39,16 @@ ui <- fluidPage("Bonjour à tous!",# On ajoute du texte
 
 
 server <- function(input, output) {
+  
+  output$hist <- renderPlot({
+    
+    titre <- "Histogramme Iris Sepal Length"
+    
+    hist(iris$Sepal.Length, 
+         main = titre, 
+         breaks = input$num) # input$num permet de faire varier le nombre de cells
+                              # se met automatiquementà jour sur le Shiny
+  })
   
 }
 shinyApp(ui = ui, server = server)
